de84d705c59a4054d354d3b04aa191653824b2cb
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-12 22:14:26 +0100

    c server style

---
+++ cserver.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+	printf("Content-type:text/html\r\n\r\n");
+	printf("<html<head><title>c server</title></head><body><h1> \n\n");
+	printf("Hello world from c server \n\n");
+	printf("</h1></body></html \n\n");
+	}
