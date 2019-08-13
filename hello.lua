26e261a6024a96bab660e98a0caa509f3d33b160
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-13 12:46:16 +0100

    lua server sample

---
+++ hello.lua
@@ -0,0 +1,5 @@
+#!/usr/bin/lua
+print ("Content-type:text/html\r\n\r\n")
+print ("<html><head><title>lua server</title></head><body><h1>")
+print ("hello world from lua.!")
+print ("</h1></body></html>")
