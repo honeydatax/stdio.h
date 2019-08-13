bc4645e0ba9d4e25f88d657beaf2f10f37eadeab
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-13 18:53:36 +0100

    ruby server sample

---
+++ hello.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby  -w
+put  "Content-type:text/html\r\n\r\n";
+put "<html><head><title>ruby server</title></head><body><h1>";
+put "hello world from ruby.!";
+put "</h1></body></html>";
