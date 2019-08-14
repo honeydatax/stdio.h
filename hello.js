eb971f5b05d1574e686fbb7db814b147cbb7de4e
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-14 13:53:18 +0100

    console node.js server

---
+++ hello.js
@@ -0,0 +1,5 @@
+
+		console.log("Content-type:text/html\r\n\r\n");
+		console.log("<html><head><title>javascript server</title></head><body><h1>");
+		console.log("hello world from java script console .!\n");
+		console.log("</h1></body></html>");
