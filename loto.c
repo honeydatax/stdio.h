38ac819fa60f80e291f0adf23790835a60518187
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-14 11:22:04 +0100

    loto class call shell

---
+++ loto.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main(){
+	char ss[512];
+	char s[255];
+	strcpy(ss,"java loto ");
+	chdir("cgi-bin\0");
+	system(ss);
+	}
