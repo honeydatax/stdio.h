6c6780c20935af96f8d033668f26cc23cfa8c788
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-17 21:53:34 +0100

    eval awk using

--- eval.c
+++ eval.c
@@ -4,14 +4,87 @@
 #include <string.h>
 
 
+void head();
+void tail();
+void spaces(char* c, char* cc);
+
+
 int main(){
-	char ss[512];
-	char s[255];
-	printf("Content-type:text/text\r\n\r\n\r\n\r");
-	strcpy(ss,"/usr/bin/awk 'BEGIN { print (");
+	long long i;
+	long long ii;
+	long long iii;
+	long long iiii;
+	int a;
+	char ssss[9000];
+	char ss[9000];
+	char s[9000];
+	char sss[9000];
+	FILE *f1;
+	head();
+	chdir("cgi-bin\0");
 	strcpy(s,getenv("QUERY_STRING"));
-	printf("\n\n\n%s = \n\n\n\n",s);
-	strcat(ss,s);
-	strcat(ss," ) } '\0");
-	system(ss);
+	printf("<p> %s  </p>\n",s);
+    strcpy(ss,"awk 'BEGIN{ print ");
+    //spaces(s,sss);
+    strcat(ss,s);
+	strcat(ss," } ' > stdout ");
+    system(ss);
+	f1=fopen("stdout","r");
+	i=ftell(f1);
+	fgets(s,255,f1);
+	fclose(f1);
+	printf("<p> = %s  </p>\n",s);
+    tail();
 	}
+
+// head html http
+void head(){
+	printf("Content-type:text/html\r\n\r\n");
+	printf("<html<head><title>c server</title></head><body><h1> \n\n");
+	}
+
+
+// tail html http	
+void tail(){
+	printf("</h1></body></html \n\n");
+	}
+
+
+void spaces(char* c, char* cc){
+	int i;
+	int ii;
+	int iii;
+	
+	ii=512;
+	iii=0;
+	for (i=0;i<ii;i++){
+		cc[ii]=c[i];
+		if (c[i]=='+' || c[i]=='-' || c[i]=='/') {
+			cc[iii]=' ';
+			iii++;
+			cc[iii]=c[i];
+			iii++;
+			cc[iii]=' ';
+			iii++;
+		}
+
+		if (c[i]=='*') {
+			cc[iii]=' ';
+			iii++;
+			cc[iii]='\\';
+			iii++;
+			cc[iii]='*';
+			iii++;
+			cc[iii]=' ';
+			iii++;
+		}
+		if (c[i]!='+' && c[i]!='-' && c[i]!='/' && c[i]!='*') {
+			cc[iii]=c[i];
+			iii++;
+		}
+		
+		if (c[i]==0) {
+			i=513;
+		}
+	}
+}
