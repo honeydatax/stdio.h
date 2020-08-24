4990b991cb2072bb06d04a1325a2a3168706860d
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-24 13:52:54 +0100

    Delete restart.java

--- restart.java
+++
@@ -1,8 +0,0 @@
-public class restart{
-	public static void main(String[] args){
-		System.out.println("Content-type:text/html\r\n\r\n");
-		System.out.println("<html><head><meta http-equiv='Refresh' content='1 ; url=/cgi-bin/fatal_error.sh'><title>java server</title></head><body><h1>");
-		System.out.println("fatal error .!\n");
-		System.out.println("</h1></body></html>");
-	}
-}
