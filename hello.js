bde9a80b54d96115e796f2039a9da364ad798b48
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-24 14:21:11 +0100

    Delete hello.js

--- hello.js
+++
@@ -1,5 +0,0 @@
-
-		console.log("Content-type:text/html\r\n\r\n");
-		console.log("<html><head><title>javascript server</title></head><body><h1>");
-		console.log("hello world from java script console .!\n");
-		console.log("</h1></body></html>");
