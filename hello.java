e2313b3de2706e0fddca7e338ca584726df8711c
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-24 14:21:36 +0100

    Delete hello.java

--- hello.java
+++
@@ -1,8 +0,0 @@
-public class hello{
-	public static void main(String[] args){
-		System.out.println("Content-type:text/html\r\n\r\n");
-		System.out.println("<html><head><title>java server</title></head><body><h1>");
-		System.out.println("hello world .!\n");
-		System.out.println("</h1></body></html>");
-	}
-}
