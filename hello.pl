02e7840b47afae1208031e247767599fe5f7bc43
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-24 14:20:12 +0100

    Delete hello.pl

--- hello.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-print "Content-type:text/html\r\n\r\n";
-print "<HTML>\n<HEAD>\n<TITLE>perl server</TITLE>\n</HEAD>\n<BODY>\n<H1>\n";
-$hello="hello world .!";
-print "$hello \n";
-print "\n</H1>\n</BODY>\n</HTML>\n";
-
