35a3fe19d1505601cda615eb34129c046f53ed70
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-24 13:52:25 +0100

    Delete restart_mail.sh

--- restart_mail.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Adim then server as restart.!:the server restart or starts\n for start up or same serias error." > cgi-bin/restart.dat
-mail -s "Adim then server as restart.!" "AdimUser1@gmail.com" <  cgi-bin/restart.dat
-mail -s "Adim then server as restart.!" "AdimUser2@gmail.com" <  cgi-bin/restart.dat
