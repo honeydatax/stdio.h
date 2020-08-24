5af19f7bce697a4a4e96038450b2bd691f4d9713
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-24 14:19:42 +0100

    Delete hello.rb

--- hello.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby  -w
-puts  "Content-type:text/html\r\n\r\n";
-puts "<html><head><title>ruby server</title></head><body><h1>";
-puts "hello world from ruby.!";
-puts "</h1></body></html>";
