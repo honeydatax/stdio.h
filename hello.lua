35e51b74acfa8e255d5d264929bd1f1d901f5569
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-24 14:20:45 +0100

    Delete hello.lua

--- hello.lua
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/lua
-print ("Content-type:text/html\r\n\r\n")
-print ("<html><head><title>lua server</title></head><body><h1>")
-print ("hello world from lua.!")
-print ("</h1></body></html>")
